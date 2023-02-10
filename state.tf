178190191bce2d7c62369be3a68c6ed79dbfacc8
raviteja <ravitejatfcdemo@gmail.com>
2023-02-10 13:28:39 +0530

    Terraform

--- state.tf
+++ state.tf
@@ -3,3 +3,5 @@ terraform {
 }
 
 # remaining values are coming from state.tfvars
+# we cannot variablize the terraform state.tf file according to the ENVIRONMENT we needed
+# so, based on ENVIRONMENT we are running, we are going to pick that ENVIRONMENT BACKEND FILES 'state.tfvars'
