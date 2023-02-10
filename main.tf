b81b4a3126ba2ea864c4aaba5b5bfcfa3ebf9a3f
raviteja <ravitejatfcdemo@gmail.com>
2023-02-10 18:52:04 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ module "network" {
   for_each = var.vpc
   cidr_block = each.value.cidr_block
 }
+# there is this one module and we are calling this module
+
 # To Refer Particular Module
 # Everything we need to maintain is modules from git-side
 # we are trying to maintain the uniformity of the variables also
