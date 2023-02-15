a1bee333a56b56de9a144b7200ffc79b808a765f
raviteja <ravitejatfcdemo@gmail.com>
2023-02-15 15:43:05 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ module "subnets" {
   availability_zone = each.value.availability_zone
   name = each.value.name
   vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
 }
 
 # we call this module
