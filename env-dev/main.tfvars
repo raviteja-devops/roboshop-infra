370ccbb4565c741fa9dd7a07abdd9873aeaabcb5
raviteja <ravitejatfcdemo@gmail.com>
2023-02-16 10:53:55 +0530

    Terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -5,38 +5,30 @@ default_vpc_id = "vpc-0380e34c4b82831a1"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
-  }
-}
-
-
-subnets = {
-  public = {
-    name = "public"
-    vpc_name = "main"
-    cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
     availability_zone = ["us-east-1a", "us-east-1b"]
-    internet_gw = true
-  }
-  web = {
-    name = "web"
-    vpc_name = "main"
-    cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
-    availability_zone = ["us-east-1a", "us-east-1b"]
-    nat_gw = true
-  }
-  app = {
-    name = "app"
-    vpc_name = "main"
-    cidr_block = ["10.0.4.0/24", "10.0.5.0/24"]
-    availability_zone = ["us-east-1a", "us-east-1b"]
-    nat_gw = true
-  }
-  db = {
-    name = "db"
-    vpc_name = "main"
-    cidr_block = ["10.0.6.0/24", "10.0.7.0/24"]
-    availability_zone = ["us-east-1a", "us-east-1b"]
-    nat_gw = true
+    subnets = {
+      public = {
+        name = "public"
+        cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
+        internet_gw = true
+        create_nat_gw = true
+      }
+      web = {
+        name = "web"
+        cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
+        nat_gw = true
+      }
+      app = {
+        name = "app"
+        cidr_block = ["10.0.4.0/24", "10.0.5.0/24"]
+        nat_gw = true
+      }
+      db = {
+        name = "db"
+        cidr_block = ["10.0.6.0/24", "10.0.7.0/24"]
+        nat_gw = true
+      }
+    }
   }
 }
 
--- main.tf
+++ main.tf
@@ -5,23 +5,25 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value.cidr_block
-}
-
-
-module "subnets" {
-  source = "github.com/raviteja-devops/tf-module-subnets.git"
-  env = var.env
-  default_vpc_id = var.default_vpc_id
-
-  for_each = var.subnets
-  cidr_block = each.value.cidr_block
+  subnets = each.value.subnets
   availability_zone = each.value.availability_zone
-  name = each.value.name
-  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-  internet_gw = lookup(each.value, "internet_gw", false)
-  nat_gw = lookup(each.value, "nat_gw", false)
 }
+
+
+#module "subnets" {
+#  source = "github.com/raviteja-devops/tf-module-subnets.git"
+#  env = var.env
+#  default_vpc_id = var.default_vpc_id
+#
+#  for_each = var.subnets
+#  cidr_block = each.value.cidr_block
+#  availability_zone = each.value.availability_zone
+#  name = each.value.name
+#  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
+#  internet_gw = lookup(each.value, "internet_gw", false)
+#  nat_gw = lookup(each.value, "nat_gw", false)
+#}
 
 # looking for internet_gw, if there use that value, if not there take false
 # we call this module
