d061cb5e63675d0611759811f0c82d000a18d460
raviteja <ravitejatfcdemo@gmail.com>
2023-02-25 12:03:42 +0530

    Terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -62,9 +62,9 @@ elasticache = {
   main = {
     vpc_name = "main"
     subnets_name = "db"
-    num_node_groups         = 2
-    replicas_per_node_group = 1
-    node_type = "cache.t3.micro"
+    num_cache_nodes = 1
+    node_type       = "cache.t3.micro"
+    engine_version  = "6.x"
   }
 }
 
--- main.tf
+++ main.tf
@@ -48,9 +48,9 @@ module "elasticache" {
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
   vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
   allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
-  num_node_groups         = each.value.num_node_groups
-  replicas_per_node_group = each.value.replicas_per_node_group
-  node_type = each.value.node_type
+  num_cache_nodes = each.value.num_cache_nodes
+  node_type       = each.value.node_type
+  engine_version  = each.value.engine_version
 }
 
 
